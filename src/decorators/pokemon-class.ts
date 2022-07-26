5c3c12fff3b8ca34b05bffab8d6e7215df2b18bc
Fernando Nicolás <fersanti2896@gmail.com>
2022-07-25 22:00:29 -0500

    Decorador en métodos en pokemon-class.ts

--- src/decorators/pokemon-class.ts
+++ src/decorators/pokemon-class.ts
@@ -17,6 +17,21 @@ const blockPrototype = function( constructor: Function ) {
     Object.seal( constructor.prototype );
 }
 
+/* Está destinado a trabajar con métodos */
+function checkValidPokemonId() {
+    return function( target: any, propertyKey: string, descriptor: PropertyDescriptor ) {
+        const originalMethod = descriptor.value;
+        
+        descriptor.value = ( id: number ) => {
+            if( id < 1 || id > 800 ) {
+                return console.error('El pokemon debe estar entre 1 a 800!')
+            } else {
+                return originalMethod( id );
+            }
+        }
+    }
+}
+
 @blockPrototype
 @printToConsoleConditional( true )
 export class Pokemon {
@@ -25,4 +40,9 @@ export class Pokemon {
     constructor(
         public name: string
     ) {}
+
+    @checkValidPokemonId()
+    savePokemonToDB( id: number ) {
+        console.log( `Pokemon guardado en BD: ${ id }` )
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { Pokemon } from "./decorators/pokemon-class";
 
 const charmander = new Pokemon('Charmander');
 
-(Pokemon.prototype as any).customName = 'Pikachu'
+// (Pokemon.prototype as any).customName = 'Pikachu'
 
-console.log( charmander );
+charmander.savePokemonToDB(500);
